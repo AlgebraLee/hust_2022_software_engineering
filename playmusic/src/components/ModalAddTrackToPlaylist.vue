1fc00f8c406486a3d4e0461dd304274355d92dcc
Eureka <algebrali1218@gmail.com>
2022-10-19 03:09:41 +0800

    null

--- playmusic/src/components/ModalAddTrackToPlaylist.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        dsa
-    </div>
-</template>
--- playmusic/src/components/preload.js
+++
@@ -1,9 +0,0 @@
-const {contextBridge, ipcRenderer} = require('electron')
-
-contextBridge.exposeInMainWorld('versions', {
-    node: () => process.versions.node,
-    chrome: () => process.versions.chrome,
-    electron: () => process.versions.electron,
-    ping: () => ipcRenderer.invoke('ping')
-    // 能暴露的不仅仅是函数，我们还可以暴露变量
-})
