a81346e56d5d95baaf81b548e364b983ce65dfe6
Eureka <algebrali1218@gmail.com>
2022-10-21 13:24:23 +0800

    MusicPlayer and LoginBar

--- playmusic/src/App.vue
+++ playmusic/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <MusicPlayer/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import MusicPlayer from './components/MusicPlayer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    MusicPlayer
   }
 }
 </script>
