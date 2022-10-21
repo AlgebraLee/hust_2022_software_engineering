dd6d8a5af5f652108745c7055d2c36479321ec7c
Eureka <algebrali1218@gmail.com>
2022-10-21 14:47:36 +0800

    null

--- playmusic/src/App.vue
+++ playmusic/src/App.vue
@@ -1,34 +1,22 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <MusicPlayer/>
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-import MusicPlayer from './components/MusicPlayer.vue'
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    // HelloWorld,
-    MusicPlayer
+    HelloWorld
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- playmusic/src/main.js
+++ playmusic/src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './plugins/element.js'
 
 createApp(App).mount('#app')
