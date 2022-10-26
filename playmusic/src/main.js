23a05fcac4c2c3fefdb231ddffefbda02ea2a7d0
Eureka <algebrali1218@gmail.com>
2022-10-26 11:26:57 +0800

    track.js

--- playmusic/src/main.js
+++ playmusic/src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// import axios from 'axios'
+// axios.defaults.baseURL = '/api'
+
 createApp(App).mount('#app')
--- playmusic/vue.config.js
+++ playmusic/vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  productionSourceMap: false, // 生产环境打包不输出
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'https://api-sooty-nu.vercel.app/',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '',
+        }
+      }
+    }
+  }
 })
