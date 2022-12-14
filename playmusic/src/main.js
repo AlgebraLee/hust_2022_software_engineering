00bb6eca152dedf6999a9872c2e4870a645c2047
Eureka <algebrali1218@gmail.com>
2022-12-14 23:13:29 +0800

    null

--- playmusic/src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import ElementPlus from 'element-plus'
-import router from '@/router/index'
-
-createApp(App).use(ElementPlus).use(router).mount('#app')
